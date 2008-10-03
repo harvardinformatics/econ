4f2c44274b9d2a692328c1e5cc1de901cf3d62b6
mkelly <mkelly@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2008-10-03 16:43:08 +0000

    BugID: 59

--- flare/app/controllers/browse_controller.rb
+++ flare/app/controllers/browse_controller.rb
@@ -17,14 +17,13 @@ class BrowseController < ApplicationController
   def index
     # TODO Add paging and sorting
     @info = solr(Solr::Request::IndexInfo.new) # TODO move this call to only have it called when the index may have changed
-    @facet_fields = @info.field_names.find_all {|v| v =~ /_facet$/}
+    @econ_facet_fields = @info.field_names.find_all {|v| v =~ /_facet$/}
 
-    facet_sort_hash = {
+    @facet_sort_hash = {
       "family_facet" => 1,
-      "collection_facet=" => 2,
+      "collection_facet" => 2,
       "country_facet" => 3,
       "state_facet" => 4,
-      "classification_facet" => 5,
       "material_facet" => 6,
       "is_processed_facet" => 7,
       "specimen_format_facet" => 8,
@@ -32,22 +31,17 @@ class BrowseController < ApplicationController
       "tdwg2_facet" => 10,
     }
 
-    #@facet_fields = @facet_fields.sort { |a,b| facet_sort_hash[a] <=> facet_sort_hash[b] }
-
     @econ_facet_labels = {
       "family_facet" => "Family",
-      "collection_facet=" => "Collection",
+      "collection_facet" => "Collection",
       "country_facet" => "Country",
       "state_facet" => "State",
-      "classification_facet" => "Classification",
-      "material_facet" => "Material",
+      "material_facet" => "Classification of Material",
       "is_processed_facet" => "Is Processed?",
       "specimen_format_facet" => "Specimen Format",
       "tdwg1_facet" => "TDWG Level 1",
       "tdwg2_facet" => "TDWG Level 2",
     }
-
-
 
     @text_fields = @info.field_names.find_all {|v| v =~ /_text$/}
     
@@ -62,7 +56,7 @@ class BrowseController < ApplicationController
                                           :filter_queries => filters,
                                           :rows => @results_per_page,
                                           :start => @start,
-                                          :facets => {:fields => @facet_fields, :limit => 20 , :mincount => 1, :sort => :count, :debug_query=>true},
+                                          :facets => {:fields => @econ_facet_fields, :limit => 20 , :mincount => 1, :sort => :count, :debug_query=>true},
                                           :highlighting => {:field_list => @text_fields})
     # logger.info({:query => query, :filter_queries => filters}.inspect)
     @response = solr(request)
